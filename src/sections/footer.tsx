3dd46c1d670c48c10406adfb80a70b1eb72ac5ec
dylan <dylan.song@gmail.com>
2022-06-23 11:00:07 -0700

    220623110006

--- src/sections/footer.tsx
+++ src/sections/footer.tsx
@@ -3,7 +3,8 @@ export const Footer = () => {
     <div className="px-12 mt-8 flex flex-col bg-gradient-to-br from-blue-600 to-green-500 py-2 rounded-t-lg items-center justify-center w-full">
       <div className="font-robo text-xs text-white">
         This resume was designed through React.js, tailwindcss, the online
-        version is hosted at Aws Amplify, and the source code is
+        version is hosted at Aws Amplify
+        https://main.d1kpwr4bmug99l.amplifyapp.com/ , and the source code is at
         https://github.com/dylansong/resume2022
       </div>
     </div>
