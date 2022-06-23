1249769a53508d09a93a793cb9cd278ed5b17e39
dylan <dylan.song@gmail.com>
2022-06-23 11:04:38 -0700

    220623110438

--- src/sections/footer.tsx
+++ src/sections/footer.tsx
@@ -1,11 +1,22 @@
 export const Footer = () => {
   return (
-    <div className="px-12 mt-8 flex flex-col bg-gradient-to-br from-blue-600 to-green-500 py-2 rounded-t-lg items-center justify-center w-full">
+    <div className="px-12 mt-8 flex flex-col bg-gradient-to-br from-blue-600 to-green-500 py-2 rounded-lg items-center justify-center w-full">
       <div className="font-robo text-xs text-white">
         This resume was designed through React.js, tailwindcss, the online
         version is hosted at Aws Amplify
-        https://main.d1kpwr4bmug99l.amplifyapp.com/ , and the source code is at
-        https://github.com/dylansong/resume2022
+        <a
+          href="https://main.d1kpwr4bmug99l.amplifyapp.com/"
+          className="bg-white text-pri ml-1 px-2"
+        >
+          Click To Visit
+        </a>{" "}
+        , and the source code is at Github.{" "}
+        <a
+          href="https://github.com/dylansong/resume2022"
+          className="bg-white text-pri m1-1 px-2"
+        >
+          Click To Visit
+        </a>
       </div>
     </div>
   );
